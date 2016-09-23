4975c8d992a9cb9efa26f45128c4b0273acdfd6c
Gregor Woiwode <gw@co-it.eu>
2016-09-23 23:06:34 +0200

    Sets up demo test

---
+++ src/hypermedia-flow.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+describe('Add', () => {
+  it('add 1 to 1', () => {
+    expect(1 + 1).eq(2);
+  });
+
+  it('add 1 to 1', () => {
+    expect(1 + 1).eq(2);
+  });
+});
